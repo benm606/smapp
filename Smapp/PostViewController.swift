23aec553cbe481df3aeff4ee7bad676db9e2dce2
Ben Mesnik <Ben@Bens-MacBook-Pro.local>
2017-01-06 13:03:10 -0500

    beginning 1/6 making sure saved from yesterday, posting added

--- Smapp/PostViewController.swift
+++ Smapp/PostViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 import Firebase
 import FirebaseDatabase
+import FirebaseStorage
 
-class PostViewController: UIViewController {
+class PostViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentTextView: UITextView!
+    @IBOutlet weak var previewImageView: UIImageView!
+    @IBOutlet weak var selectImageButton: UIButton!
     
+    var imageFileName = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +31,23 @@ class PostViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        //runs when photo selection is cancelled
+        picker.dismiss(animated: true, completion: nil)   //close image picker
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        //runs when photo is picked from library
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as?
+            UIImage{
+            self.previewImageView.image = pickedImage
+            self.selectImageButton.isEnabled = false  //hide select image button
+            self.selectImageButton.isHidden = true
+            uploadImage(image: pickedImage)
+            picker.dismiss(animated: true, completion: nil)  //close image picker
+        }
+    }
+    
     @IBAction func postTapped(_ sender: Any) {     //post to firebase databse
         
         if let uid = FIRAuth.auth()?.currentUser?.uid{   ///make sure all fields r filled and create postable object
@@ -35,7 +56,8 @@ class PostViewController: UIViewController {
                     let postObject: Dictionary<String, Any> = [
                         "uid" : uid,
                         "title" : title,
-                        "content" : content
+                        "content" : content,
+                        "image" : imageFileName
                     ]
                     
                     FIRDatabase.database().reference().child("posts").childByAutoId().setValue(postObject)
@@ -52,6 +74,44 @@ class PostViewController: UIViewController {
      
     }
 
+    @IBAction func selectImageTapped(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func uploadImage(image: UIImage){                           //create random name to save photo under
+        let randomName = randomStringWithLength(length: 10)
+        let imageData = UIImageJPEGRepresentation(image, 1.0)
+        let uploadRef = FIRStorage.storage().reference().child("images/\(randomName).jpg")
+        
+        let uploadTask = uploadRef.put(imageData!, metadata: nil){
+            metadata, error in
+            
+            if error == nil{
+                //success
+                print("Successfully uploaded picture")
+                self.imageFileName = "\(randomName as String).jpg"
+            }else{
+                //error
+                print("Error uloading image: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+    func randomStringWithLength(length: Int) -> NSString{
+        let characters: NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        var randomString: NSMutableString = NSMutableString(capacity: length)
+        for i in 0..<length{
+            var len = UInt32(characters.length)
+            var rand = arc4random_uniform(len)
+            randomString.appendFormat("%C", characters.character(at: Int(rand)))
+        }
+        return randomString
+        
+    }
+    
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {   //when touched close keyboard
         view.endEditing(true)
         super.touchesBegan(touches, with: event)
